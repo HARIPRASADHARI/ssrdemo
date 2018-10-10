86041b4bbf018f29d53e1c151660c9ba79224521
HARIPRASADHARI <prasad19.hari@gmail.com>
2018-10-10 07:51:15 +0530

    ssr server code modify

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { PLATFORM_ID, APP_ID} from '@angular/core';
+import { isPlatformServer,isPlatformBrowser } from '@angular/common';
+import * as CanvasJS from './canvasjs.min';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'my-app';
+  constructor(@Inject(PLATFORM_ID) private platformId: any){}
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)){
+  let chart = new CanvasJS.Chart("chartContainer", {
+		animationEnabled: true,
+		exportEnabled: true,
+		title: {
+			text: "Basic Column Chart in Angular"
+		},
+		data: [{
+			type: "column",
+			dataPoints: [
+				{ y: 71, label: "Apple" },
+				{ y: 55, label: "Mango" },
+				{ y: 50, label: "Orange" },
+				{ y: 65, label: "Banana" },
+				{ y: 95, label: "Pineapple" },
+				{ y: 68, label: "Pears" },
+				{ y: 28, label: "Grapes" },
+				{ y: 34, label: "Lychee" },
+				{ y: 14, label: "Jackfruit" }
+			]
+		}]
+	});
+		
+	chart.render();
+}
+}
 }
